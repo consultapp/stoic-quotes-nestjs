474e381304d574cacf8c0af0bf7e5fe404f6f903
Dmitry Egorov <info@consultapp.ru>
2024-11-07 15:01:58 +0300

    docker + cron

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM  node:20.18-alpine3.20
+RUN npm set strict-ssl false
+
+WORKDIR /src
+
+COPY package.json .
+COPY package-lock.json .
+
+COPY quotes.json .
+COPY ./dist ./dist
+
+# COPY ./node_modules ./node_modules
+RUN npm config set legacy-peer-deps true
+RUN npm install
+
+# RUN ls -lash
+
+ENV PORT=3000
+
+EXPOSE $PORT
+
+ENTRYPOINT ["npm", "run",  "start:prod"]
--- src/tg.service.ts
+++ src/tg.service.ts
@@ -1,12 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectBot, Start, Update } from 'nestjs-telegraf';
 import { Context, Telegraf } from 'telegraf';
 import { QuoteService } from './quote.service';
 import { isQuote } from './lib';
 import { ConfigService } from '@nestjs/config';
 
-let cronString: string = '0 9 * * * *';
+// CronExpression
+let cronString: string = '0 */10 * * * *';
 
 @Update()
 @Injectable()
@@ -20,14 +21,23 @@ export class TgService {
     this.channel = this.configService.get<string>('TELEGRAM_CHANNEL');
     cronString =
       this.configService.get<string>('CRON_TIME_CONFIG') ?? cronString;
+
+    console.log('-> Tg channel:', this.channel);
+    console.log('-> Cron cfg:', cronString);
+    console.log(
+      'CronExpression.EVERY_30_SECONDS',
+      CronExpression.EVERY_30_SECONDS,
+    );
   }
   getData(): { message: string } {
     return { message: 'Welcome to server!' };
   }
 
   @Cron(cronString)
+  // @Cron(CronExpression.EVERY_30_SECONDS)
   sendRandomQuote() {
     const quote = this.quoteService.getRandomQuote();
+    console.log('quote', quote);
     if (isQuote(quote)) {
       return this.bot.telegram.sendMessage(
         this.channel,
